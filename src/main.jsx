0f8501f5c3a26ca86d9384103a32ee809e3493cd
ashtilawat23 <ashtilawat23@gmail.com>
2024-06-24 20:51:03 -0400

    added auth0 and chakra providers

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,32 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App.jsx'
-import './index.css'
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import App from './App.jsx';
+import { Auth0Provider } from '@auth0/auth0-react';
+import { ChakraProvider, extendTheme } from '@chakra-ui/react';
 
-ReactDOM.createRoot(document.getElementById('root')).render(
+const auth0Domain = import.meta.env.VITE_AUTH0_DOMAIN;
+const auth0ClientId = import.meta.env.VITE_AUTH0_CLIENT_ID;
+
+const config = {
+  initialColorMode: 'dark',
+  useSystemColorMode: false,
+}
+
+const theme = extendTheme({ config })
+
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(
   <React.StrictMode>
-    <App />
+    <Auth0Provider
+      domain={auth0Domain}
+      clientId={auth0ClientId}
+      authorizationParams={{
+        redirect_uri: window.location.origin,
+      }}
+    >
+      <ChakraProvider theme={theme}>
+        <App />
+      </ChakraProvider>
+    </Auth0Provider>
   </React.StrictMode>,
-)
+);
